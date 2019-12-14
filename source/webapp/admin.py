12731a460c7d238cb8490be35bb940414137582b
alisher <shera.rashiduulu@gmail.com>
2019-12-14 15:06:08 +0600

    Created  all required fields in models.py and added to admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import Photo, Comment
 
-# Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'image', 'signature', 'created_at', 'like_amount', 'author']
+    list_filter = ['created_at']
+    search_fields = ['signature', 'created_at']
+    fields = ['image', 'signature', 'created_at', 'like_amount', 'author']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Comment)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='gallery_images', null=True, blank=True, verbose_name='Фото')
+    signature = models.CharField(max_length=200, verbose_name='Подпись')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+    like_amount = models.IntegerField(default=0, verbose_name='Лайк')
+    author = models.ForeignKey(User, max_length=50, verbose_name='Кто добавил', related_name='photos_author', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.signature
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Текст')
+    image = models.ForeignKey(Photo, max_length=200, verbose_name='Фотография', related_name='comments_photo', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=False, blank=False, related_name='comments_author', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.text
